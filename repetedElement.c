0e31eb97fbfa49fd906db2dc334275850292e8a1
Aniket Karse <59339659+AniketKarse@users.noreply.github.com>
2026-08-12 22:26:56 +0530

    Improve user prompts and validate array input
    Updated prompts and added check for empty array.

--- repetedElement.c
+++ repetedElement.c
@@ -2,9 +2,14 @@
 
 int main(){
 	int n;
-	printf("enter here - ");
+	printf("enter size of array - ");
 	scanf("%d",&n);
 	int arr[n];
+	printf("Enter the array elements");
+	if(arr == 0 || n == 0){
+		printf("array is empty or undefined");
+		return 0;
+	}
 	for(int i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	}
