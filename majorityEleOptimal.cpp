3e591415e501a27c3f3b4edd4280216af0cebb5f
Aniket Karse <59339659+AniketKarse@users.noreply.github.com>
2026-08-16 12:29:14 +0530

    Add majorityElement function using Boyer-Moore algorithm
    Implement Boyer-Moore Voting Algorithm to find majority element.

---
+++ majorityEleOptimal.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int ele;
+        for(int i=0;i<n;i++){
+            if(count == 0){
+                count = 1;
+                ele = nums[i];
+            }
+            else if(nums[i] == ele){
+                count++;
+            }
+            else count--;
+        }
+        count = 0;
+        for(int i=0;i<n;i++){
+            if(ele == nums[i])count++;
+        }
+        if(count > n/2){
+            return ele;
+        }
+        return -1;
+    }
+};
