43ab95dd5aa6e032f14bb3ff2b14d6d49686edcc
Aniket Karse <59339659+AniketKarse@users.noreply.github.com>
2026-08-23 21:46:38 +0530

    Implement Fibonacci sequence using recursion

---
+++ fibonacci .cpp
@@ -0,0 +1,12 @@
+class Solution {
+private:
+    int rec(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return rec(n-1)+rec(n-2);
+    }
+public:
+    int fib(int n) {
+        return rec(n);
+    }
+};
