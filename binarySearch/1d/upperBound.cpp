8f8acff7efc3f95167bdbdbe99e2437451fff5d1
Aniket Karse <59339659+AniketKarse@users.noreply.github.com>
2026-08-29 00:28:40 +0530

    Implement upperBound function for binary search

---
+++ binarySearch/1d/upperBound.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0,high=n-1;
+        int mid;
+        while(low <= high){
+            mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+            if(low == mid || high == mid) return mid;
+        }
+        return mid;
+    }
+};
