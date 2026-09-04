014ab3c2392f0800c6a2c5e199d57be43ffeac5c
Aniket Karse <59339659+AniketKarse@users.noreply.github.com>
2026-09-04 17:35:12 +0530

    Refactor powrec to use long long for exponent
    i found it better

--- recursion/gettingaHold/powImplimentationRecursion.cpp
+++ recursion/gettingaHold/powImplimentationRecursion.cpp
@@ -1,23 +1,22 @@
 class Solution {
 private:
-    double powrec(double x, int n){
+    double powrec(double x, long long n){
         if(n == 0) return 1;
         if(n == 1) return x;
         else{
             if(n%2){
-                if(n < 0)return x*powrec(x,n+1);
-                else return x*powrec(x,n-1);
+                return x*powrec(x,n-1);
             }else {
               return powrec(x*x,n/2);
             }
-        double ans;
-        return ans;
         }
     }
 public:
     double myPow(double x, int n) {
         double ans = 1.0;
-        ans = powrec(x,n);
+        long long nn = n;
+        if(nn < 0) nn = -1 * nn;
+        ans = powrec(x,nn);
 
         if(n < 0){
             ans = (double)(1.0)/(double)(ans);
